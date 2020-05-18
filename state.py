c26453a2b1a8f0f4f266417bf2068c65eecf66bc
Margiris <34870816+Margiris@users.noreply.github.com>
2020-05-18 09:54:01 +0300

    Update state.py

--- state.py
+++ state.py
@@ -337,7 +337,7 @@ class State:
         if props[0] == "P":
             model_filename = props[11] if len(props) == 13 else None
             self.previous_state.main_player = Player((self.previous_state.all_sprites, self.previous_state.player_sprites), self.previous_state.acknowledge_death, self.previous_state.surfaces[0].tile_size, color, (int(props[2]), int(
-                props[3])), (int(props[4]), int(props[5])), goal=self.previous_state.goal, walls=self.previous_state.wall_sprites, fpm=int(props[7]), move_ticks=int(props[8]), reached_goal=props[10] == "True", vision_surface=self.previous_state.surfaces[0], model_name=model_filename)
+                props[3])), (int(props[4]), int(props[5])), goal=self.previous_state.goal, walls=self.previous_state.wall_sprites, fpm=int(props[7]), move_ticks=int(props[8]), reached_goal=props[10] == "True", vision_surface=self.previous_state.surfaces[0])
             self.previous_state.alive_count += 1
             if props[9] != "True":
                 self.previous_state.main_player.die()
